063859c5b45a501bd88156d32e86d27d2b852011
akippnn <80670461+akippnn@users.noreply.github.com>
2026-04-28 05:37:00 +0800

    refactor: unify version management by removing hardcoded version from build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 defaultTasks("updateLicenses", "shadowJar")
 
-version = "0.52.1"
+
 if (project.hasProperty("prodVersion")) {
     version = project.property("prodVersion").toString()
 }
